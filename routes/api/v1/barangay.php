f4df4f5e197af82811a57b968f2f623f4a5f67c9
aljon <aljonkenfernandez36@gmail.com>
2026-05-19 14:28:03 +0800

    feat(routes): map endpoints for barangay map and flood simulation data

--- routes/api/v1/barangay.php
+++ routes/api/v1/barangay.php
@@ -1,19 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\v1\BarangayController;
-use App\Models\Barangay;
 use Illuminate\Support\Facades\Route;
-
-Route::get('/barangay', function () {
-    return response()->json(['message', 'hello this is barangay']);
-});
-
-Route::get('/flood-data', function () {
-    // Kunin lahat ng barangay boundaries
-    $barangays = Barangay::withGeoJson()->get();
-
-    return response()->json($barangays);
-});
 
 Route::get('/map/barangays', [BarangayController::class, 'getBarangayMap']);
 Route::get('/map/flood-simulation', [BarangayController::class, 'getFloodSimulation']);
